70161f4bc23970901416f36c45b8047a3d74c3b3
zeynep çırçır <81877446+zeynepcircir@users.noreply.github.com>
2023-05-31 16:27:06 +0300

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        $usertype=Auth::user()->usertype;
+
+        if($usertype=='1')
+        {
+            return view('admin.home');
+        }
+        
+        else
+        {
+            return view('user.home');
+        }
+    }
+
+    public function index()
+    {
+        if(Auth::id())
+        {
+            return redirect('redirect');
+        }
+
+
+        else
+        {
+            return view('user.home');
+        }
+
+        
+    }
+}
